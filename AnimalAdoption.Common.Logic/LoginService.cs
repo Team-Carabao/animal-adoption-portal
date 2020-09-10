4939bb643c8df72f14aa976a22e1f940a897ac41
unknown <gptolentino@pgpc.com.ph>
2020-09-10 06:54:35 +0000

    change plaseholder name in login services

--- AnimalAdoption.Common.Logic/LoginService.cs
+++ AnimalAdoption.Common.Logic/LoginService.cs
@@ -41,7 +41,7 @@ namespace AnimalAdoption.Common.Logic
                     name = "Murphy";
                     break;
                 case "placeholder":
-                    name = "Placeholder";
+                    name = "Carabao";
                     break;
             }
             return name;
